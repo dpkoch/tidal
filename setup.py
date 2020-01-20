734ea756d402ec1a92c026b2e4c5b287e5a3599b
Daniel Koch <daniel.p.koch@gmail.com>
2020-01-20 16:23:25 -0700

    Update setup.py to include README

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 import setuptools
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setuptools.setup(
     name="tidal-parser",
     version="0.0.1",
     author="Daniel Koch",
     author_email="daniel.p.koch@gmail.com",
     description="Python parser for binary log files written with the TiDaL C++ logging library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     license="MIT",
     url="https://github.com/dpkoch/tidal",
     packages=setuptools.find_packages(),
