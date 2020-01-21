dc5f465dbbf402e1a10a6b46a525152e30b3522b
Daniel Koch <daniel.p.koch@gmail.com>
2020-01-20 17:28:45 -0700

    Fix PyPI release configuration

--- setup.py
+++ setup.py
@@ -5,7 +5,8 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="tidal-parser",
-    version="0.0.1",
+    version="0.0.1.post1",
+    py_modules=['tidal_parser'],
     author="Daniel Koch",
     author_email="daniel.p.koch@gmail.com",
     description="Python parser for binary log files written with the TiDaL C++ logging library",
@@ -13,11 +14,18 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     license="MIT",
     url="https://github.com/dpkoch/tidal",
-    packages=setuptools.find_packages(),
+    project_urls={
+        "Bug Tracker": "https://github.com/dpkoch/tidal/issues",
+        "Documentation": "https://github.com/dpkoch/tidal",
+        "Source Code": "https://github.com/dpkoch/tidal",
+    },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+    ],
+    install_requires = [
+        'numpy'
     ],
     python_requires='>=3.6',
 )
